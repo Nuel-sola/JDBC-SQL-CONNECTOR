9682dbe790370ef73622e5b934a5989f37bb808e
Nuel <adebiyi.e@northeastern.edu>
2024-03-24 03:36:46 +0100

    new commit

---
+++ src/MainPackage/User.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainPackage;
+
+/**
+ *
+ * @author chait
+ */
+public class User {
+
+    private String Name;
+    private int Age;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+    
+    
+}
